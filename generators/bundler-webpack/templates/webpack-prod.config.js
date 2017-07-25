dd7bc7d073a8082d360517f0f323408866719f83
Tyler Fisher <tfisher@politico.com>
2017-07-25 10:10:32 -0400

    uglify config

--- generators/bundler-webpack/templates/webpack-prod.config.js
+++ generators/bundler-webpack/templates/webpack-prod.config.js
@@ -39,15 +39,10 @@ module.exports = {
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true
-      },
       compress: {
-        screw_ie8: true
+        warnings: false
       },
-      comments: false
+      sourceMap: true
     }),
     new ExtractTextPlugin('css/styles.css'),
     new OptimizeCssAssetsPlugin(),
